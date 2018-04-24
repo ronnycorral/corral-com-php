399e05c6a2692b79d5b0cdffbf7909ae8e9064a4
Ron Corral <rcorral@corral.com>
2018-04-24 02:38:24 +0000

    Initial add of colophon.php

---
+++ colophon.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+<head>
+<title>Colophon for Ron Corral's Website</title>
+<link href="/css/stylesheet.css" rel="stylesheet">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+  <link rel="manifest" href="/manifest.json">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="theme-color" content="#ffffff">
+  <meta name=viewport content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8">
+</head>
+<body>
+<h1 class="headingcenter">Colophon</h1>
+<div class="cdheaderrow cf">
+   <div class="menuicons homeicons cf">
+      <ul class="iconrowul">
+      <li><a href="/"><img class="roundcorner" height=64 width=64 alt="Home" title="Home" src='/images/home.png'></a></li>
+      </ul>
+   </div>
+   <div class="hideoverflow">
+      <div class="cdheaderleft">
+         <ul class="iconrowul">
+         <li><img class="roundcorner" src=/images/colophon.jpg height=64 width=64 alt="Colophon">
+         </ul>
+      </div>
+      <div>
+      <p>This web site is over 20 years old. The only old content that's still around is the CD Collection page which started out as Perl scripts parsing text files. Version 2.0 was still straight HTML but the Perl CGI's were replaced with PHP. This is version 2.5. With the addition of CSS and Javascript, hopefully this looks like it was written fairly recently instead of last century. Version 3.0 will be some sort of moden framework, once I decide what I feel like learning.</p>
+      </div>
+   </div>
+</div>
+
+<ul>
+<li>AWS EC2 instance using an Amazon Linux AMI
+<li>LAMP stack with Linux, Apache, MySQL and PHP
+<li>Memcached between web server and MySQL
+<li>Python for background scripts, mainly for parsing log files
+<li>HTML, CSS and Javascript
+<li>AJAX for updating the CD page content
+<li>Javascript libraries Highcharts and Highmaps for graphing
+<li>Searching? Just Swish-e
+</ul>
+<div class="cdheaderrow cf">
+   <div class="hideoverflow">
+      <div class="cdheaderleft">
+         <ul class="iconrowul">
+         <li><img class="roundcorner" src=/images/oldcorral.png height=64 width=64 alt="Colophon">
+         </ul>
+      </div>
+      <div>
+      <p>Version 1.0 was HTML and the CD Collection page was a Perl CGI that parsed through text files that were exported from an Excel spreadsheet that contained the CD and artist info. After a few months a webcam was installed which I dubbed the RonCam.</p>
+      </div>
+   </div>
+</div>
+
+</body>
+</html>
