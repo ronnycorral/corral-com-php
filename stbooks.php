4a27b9d63a54947a85ce7e48b5607eebbda3fe2b
Ron Corral <rcorral@corral.com>
2018-04-25 06:59:33 +0000

    Update includes for updatred favicon

--- stbooks.php
+++ stbooks.php
@@ -3,14 +3,15 @@
 <!doctype html>
 <html lang="en">
 <head>
-   <title>Ron Corral's list of Star Trek books to read</title>
+   <title>Ron Corral's List of Star Trek Books to Read</title>
    <link href="css/stylesheet.css" rel="stylesheet">
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-  <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-  <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-  <link rel="manifest" href="/manifest.json">
-  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-  <meta name="theme-color" content="#ffffff">
+   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+   <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+   <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+   <link rel="manifest" href="/manifest.json">
+   <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+   <meta name="msapplication-TileColor" content="#ffc40d">
+   <meta name="theme-color" content="#ffffff">
    <meta charset="UTF-8">
    <meta name=viewport content="width=device-width, initial-scale=1">
 <style>
