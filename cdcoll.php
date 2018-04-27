f6e1500411809c981f4bc0501d6eabad29cb74ab
Ron Corral <rcorral@corral.com>
2018-04-27 06:40:08 +0000

    Previous commit removed file, forgot to commit references to svg

--- cdcoll.php
+++ cdcoll.php
@@ -76,7 +76,6 @@ $artist_title = isset($artist_info['artist']) ? " - " . htmlentities($artist_inf
   <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
   <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
   <link rel="manifest" href="/manifest.json">
-  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
   <meta name="msapplication-TileColor" content="#ffc40d">
   <meta name="theme-color" content="#ffffff">
   <meta charset="UTF-8">
