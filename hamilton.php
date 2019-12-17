e55ed5f29f4634ece0f0a9e990e0c09f04a9c4fe
Ron Corral <rcorral@corral.com>
2019-12-16 21:37:48 -0800

    Adding unfurling to hamilton.php

--- hamilton.php
+++ hamilton.php
@@ -1,21 +1,33 @@
+<?php
+$server =  explode(".",$_SERVER['HTTP_HOST']);
+$domain = $server[(count($server)-2)];
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>The Hamilton Lottery Results</title>
-        <link href="css/stylesheet.css" rel="stylesheet">
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-        <link rel="manifest" href="/manifest.json">
-        <meta name="msapplication-TileColor" content="#ffc40d">
-        <meta name="theme-color" content="#ffffff">
-        <meta name=viewport content="width=device-width, initial-scale=1">
-<!--
-        <meta name=viewport content="width=device-width, initial-scale=1">
--->
-        <script type="text/javascript" src="/js/jquery-1.11.0.min.js"></script>
-	<script type="text/javascript">
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+   <title>The Hamilton Lottery Results</title>
+   <link href="css/stylesheet.css" rel="stylesheet">
+   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+   <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+   <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+   <link rel="manifest" href="/manifest.json">
+   <meta name="msapplication-TileColor" content="#ffc40d">
+   <meta name="theme-color" content="#ffffff">
+   <meta name=viewport content="width=device-width, initial-scale=1">
+   <meta property="og:type" content="website">
+   <meta property="og:site_name" content="Ron Corral">
+   <meta property="og:title" content="Ron Corral's Hamilton Lottery Results">
+   <meta property="og:image" content="http://<?php echo $domain; ?>.com/images/hamiltonpage.png">
+   <meta property="og:description" content="My wife and I both play the Hamilton Lottery and one of us is way better than the other. Find out which one.">
+   <meta property="og:url" content="https://<?php echo $domain; ?>.com/">
+   <meta name="twitter:site" content="@corral">
+   <meta name="twitter:card" content="summary">
+   <meta name="twitter:title" content="Ron Corral's Hamilton Lottery Results">
+   <meta name="twitter:description" content="My wife and I both play the Hamilton Lottery and one of us is way better than the other. Find out which one.">
+   <meta name="twitter:image" content="http://<?php echo $domain; ?>.com/images/hamiltonpage.png">
+   <script type="text/javascript" src="/js/jquery-1.11.0.min.js"></script>
+   <script type="text/javascript">
 artistscdcount= [
 'Ron','Claudia'
 ];
@@ -55,7 +67,7 @@ cdcount= [
 <div class="cdheaderrow cf">
  <div class="menuicons homeicons  cf">
       <ul class="iconrowul">
-      <li><img class="roundcorner imagepointer" src='/images/bars.png' height=64 width=64 alt="How Many Times We Won" title="How Many Times We Won" onclick="drawBarChartUrls('sitecharts', artistscdcount,cdcount,'How Many Times We Won','How Many Times We Won')"></li>
+      <li><img class="roundcorner imagepointer" src='/images/bars.png' height=64 width=64 alt="How Many Times We Won" title="How Many Times We Won" onclick="drawBarChartUrls('sitecharts', artistscdcount,cdcount,'How Many Times We Won','How Many Times Each Of Us Has Won')"></li>
       <li><img class="roundcorner imagepointer" src='/images/pie.png' height=64 width=64 alt="Percentage of Times Each of Us Has Won" title="Percentage of Times Each of Us Has Won" onclick="drawPieChart('sitecharts', sqlattempts,'Percentage of Times Each of Us Has Won','Winning Percentage')"></li>
       <li><img class="roundcorner imagepointer" src='/images/lines.png' height=64 width=64 alt="When we won" title="When we won" onclick="drawHamilton('sitecharts')"></li>
       </ul>
