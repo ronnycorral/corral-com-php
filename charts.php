41c8b978ea6635ffd5c7ca13a3226568b79e6889
Ron Corral <rcorral@corral.com>
2020-02-16 00:54:38 +0000

    change initial chart draw

--- charts.php
+++ charts.php
@@ -103,7 +103,7 @@ mysqli_close($dbc);
             ];
          </script>
 </head>
-<body onload="drawPieChart('sitecharts', sqlattempts,'Valid SQL Calls vs. SQL Injection Attempts','SQL calls made');">
+<body onload="drawVisitorMap('sitecharts', countrycodecount);">
 
 <h1 class="headingcenter">Statistics For The Site</h1>
 
